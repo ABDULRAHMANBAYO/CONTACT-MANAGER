1e3ec9da4b3e7ddf298950e0061077065446b8fe
SULTAN <ramhziworld@gmail.com>
2020-04-24 18:13:02 +0100

    Project setup

---
+++ app/src/main/java/com/example/contactmanager/data/DatabaseHandler.java
@@ -0,0 +1,38 @@
+package com.example.contactmanager.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.contactmanager.R;
+import com.example.contactmanager.util.Util;
+
+
+public class DatabaseHandler extends SQLiteOpenHelper {
+    public DatabaseHandler(Context context) {
+        super(context, Util.DATABASE_NAME, null, Util.DATABASE_VERSION);
+    }
+
+    //Create Table
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //Create table name (id,name and phonenumber)
+        String CREATE_CONTACT_TABLE = "CREATE TABLE " + Util.TABLE_NAME + "("
+                + Util.KEY_ID + "INTEGER PRIMARY KEY," +
+                Util.KEY_NAME + "TEXT," +
+                Util.KEY_PHONE_NUMBER + "TEXT" + ")";
+        db.execSQL(CREATE_CONTACT_TABLE);
+
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String DROP_TABLE = String.valueOf(R.string.db_drop);
+        db.execSQL(DROP_TABLE, new String[]{Util.DATABASE_NAME});
+
+        //Create a table again
+        onCreate(db);
+
+    }
+}
